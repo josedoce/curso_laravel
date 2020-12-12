43c17d6386545f4b4c7dfd177f095d6d8df6c3a8
josedoce <66434808+josedoce@users.noreply.github.com>
2020-12-12 16:56:11 -0300

    Update routing.php

--- routing.php
+++ routing.php
@@ -1,5 +1,46 @@
 //O route do laravel pode retornar qualquer tipo de dado atravês dessa estrutura:
 
-Route::get('/nome', function(){
-  return variavel;
+//enviando dados:
+
+Route::get('/about', function(){
+	$animais = ['gato','cachorro','galinha','veado'];
+	return $animais[0];
+});
+
+//renderizando views:
+
+Route::get('/rota', function(){
+	return view('nome_elemento_view');
+});
+
+//renderizando subviews:
+
+Route::get('/admin',function(){
+	return view('admin/admin');
+});
+
+//renderizando subvires utilizando ponto em vez da barra
+
+Route::get('/admin',function(){
+	return view('admin.admin');
+});
+
+//renderizando variaveis atraves das views
+
+//forma indireta
+Route::get('/admin',function(){
+	$animais = [
+		'mamifero'=>'Baleia',
+		'reptil'=>'Cobra'
+	];
+	
+	return view('admin.admin', $animais);
+});
+
+//forma direta
+Route::get('/admin',function(){
+	return view('admin.admin', [
+		'mamifero'=>'Baleia',
+		'reptil'=>'Cobra'
+	]);
 });
