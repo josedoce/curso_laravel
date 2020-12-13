0eb21c2ef7f0a1605ef7197fd458c4c85fc547dd
josedoce <66434808+josedoce@users.noreply.github.com>
2020-12-12 22:11:01 -0300

    Update controllers.php

--- controllers.php
+++ controllers.php
@@ -25,3 +25,36 @@ depois coloque um parametro na função dentro do metodo no controller
 function apresentarUsuario($parametrocontroler=null){
     	return "Estou na apresentacão do usuario: ".$nome;
 }
+
+//De Route para Controller e de Controller para View
+	1-rota, crie os dados e mande para o controller!
+
+		Route::get('tratar_usuario/{usuario}/{senha}','usuarioController@trateUsuario');
+	
+	2-controller, receba os dados da rota e mande para a view!
+		
+		class usuarioController extends Controller{	
+		    //mande esses dados para a view
+		    function trateUsuario($nome=null,$senha=null){
+			//$nome e $senha vem da rota;
+			$usuario_tratado = 'Bem vindo, '.$nome;
+			$senha_tratada = md5($senha);
+			return view('usuario',compact('usuario_tratado','senha_tratada'));
+		    }
+		}
+
+	3-view, exiba para o usuario os dados tratados pelo controller
+
+		//usuario
+		echo '<p>usuario: '.$usuario_tratado.'</p>';
+		//senha
+		echo '<p>senha: '.$senha_tratada.'</p>';
+
+
+//resources controller 
+	criação automatica de CRUD
+	php artisan make:controller --resource NomeController
+	//associação ao controller
+	Route::resource('usuarios','usuarioController');
+	conexão ao crud
+	php artisan route:list
