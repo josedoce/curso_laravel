f7b6d316f85df58e087d563881229e9be4dfcab3
josedoce <66434808+josedoce@users.noreply.github.com>
2020-12-15 12:31:54 -0300

    Create models.php

---
+++ models.php
@@ -0,0 +1,17 @@
+////Adaptar o Model para apresentar registos
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class noticias extends Model
+{
+    //para definir um argumento diferente do padrão da orm, basta especificar um objeto
+    protected $primaryKey = 'id_noticia';
+}
+
+get
+post
+put {{ method_field('PUT') }} necessita por no form com o methodo post
+delete {{ method_field('DELETE') }}  necessita por no form com o methodo post
