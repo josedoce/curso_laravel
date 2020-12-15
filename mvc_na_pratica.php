b55d64f463316c477d0e565005528197853dcd1c
josedoce <66434808+josedoce@users.noreply.github.com>
2020-12-15 11:03:38 -0300

    Update mvc_na_pratica.php

--- mvc_na_pratica.php
+++ mvc_na_pratica.php
@@ -1,3 +1,5 @@
 //comando para criar uma estrutura mvc rapida
 //comando php artisan make:model nomeModel -m -c --resource
-https://laravel.com/docs/8.x/eloquent
+      https://laravel.com/docs/8.x/eloquent
+
+
